5497606243cbe817f2a74840b26253668a105573
TechFounder <techfounder@outlook.com>
2013-11-15 09:55:45 -0500

    Initial commit with .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omnisti::Application.config.secret_key_base = 'c7daee6f1b8c41fcd12c09d65b41181e353e62a93a1f6f80860c90653bb334a35c191901e49d7d47a39340ad13eb2ac9b25aeff3b8551999dccf778ab250f601'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Omnisti::Application.config.session_store :cookie_store, key: '_omnisti_session'
